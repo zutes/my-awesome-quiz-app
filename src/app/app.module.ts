c56aad06a9d7b5505e6a4bdbdcbc8321d33fd7a6
zutes <zutes3479@gmail.com>
2020-08-24 14:59:59 -0600

    Updated module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, QuestionFormComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
