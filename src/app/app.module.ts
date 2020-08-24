7f2e16b383aed700bf7bf8c3a5b787fa84da4248
zutes <zutes3479@gmail.com>
2020-08-24 15:17:45 -0600

    Updated file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
 
 @NgModule({
   declarations: [AppComponent, QuestionFormComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
